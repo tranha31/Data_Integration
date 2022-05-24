b1d3a1da680b9dff37874d891dee92e2fdc96ef9
tranha31 <tranquanha31082000@gmail.com>
2022-05-25 00:32:04 +0700

    tqha-feat: update code lưu dl và db

--- tgdd_phone/convert_data/dl.py
+++ tgdd_phone/convert_data/dl.py
@@ -1,5 +1,6 @@
 import mysql.connector
-from mysql.connector import errorcode
+from mysql.connector import Error
+from pymysql import NULL
 from item import TgddPhoneItem
 
 config = {
@@ -14,33 +15,53 @@ config = {
 conn = mysql.connector.connect(**config)
 
 class DL():
+  def GetAll(self):
+      cursor = conn.cursor(dictionary=True)
+      cursor.execute("SELECT * FROM mobiphone")
+      records = cursor.fetchall()
 
-    def GetAll(self):
-        cursor = conn.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM mobiphone")
-        records = cursor.fetchall()
+      result = []
+      for row in records:
+          phone = TgddPhoneItem()
+          phone.id = row["RefID"]
+          phone.company = row["Company"]
+          phone.name = row["Name"]
+          phone.memory = row["Memory"]
+          phone.color = row["Color"]
+          phone.originPrice = row["OriginPrice"]
+          phone.discountPrice = row["DiscountPrice"]
+          phone.discountRate = row["DiscountRate"]
+          phone.screen = row["Screen"]
+          phone.operatingSystem = row["OperatingSystem"]
+          phone.frontCamera = row["FrontCamera"]
+          phone.behindCamera = row["BehindCamera"]
+          phone.chip = row["Chip"]
+          phone.ram = row["Ram"]
+          phone.sim = row["Sim"]
+          phone.pin = row["Pin"]
+          phone.imageUrl = row["ImageUrl"]
 
-        result = []
-        for row in records:
-            phone = TgddPhoneItem()
-            phone.id = row["RefID"]
-            phone.company = row["Company"]
-            phone.name = row["Name"]
-            phone.memory = row["Memory"]
-            phone.color = row["Color"]
-            phone.originPrice = row["OriginPrice"]
-            phone.discountPrice = row["DiscountPrice"]
-            phone.discountRate = row["DiscountRate"]
-            phone.screen = row["Screen"]
-            phone.operatingSystem = row["OperatingSystem"]
-            phone.frontCamera = row["FrontCamera"]
-            phone.behindCamera = row["BehindCamera"]
-            phone.chip = row["Chip"]
-            phone.ram = row["Ram"]
-            phone.sim = row["Sim"]
-            phone.pin = row["Pin"]
-            phone.imageUrl = row["ImageUrl"]
+          result.append(phone)
+      
+      cursor.close()
+      return result
+  
+  def Insert(self, item):
+    try:
+      cursor = conn.cursor()
+      cursor.callproc('Proc_Insert_Phone', item)
+      conn.commit()
+    except Error as e:
+        print(e)
+    finally:
+        cursor.close()
 
-            result.append(phone)
-            
-        return result
+  def Update(self, item):
+    try:
+      cursor = conn.cursor()
+      cursor.callproc('Proc_Update_Phone', item)
+      conn.commit()
+    except Error as e:
+        print(e)
+    finally:
+        cursor.close()
